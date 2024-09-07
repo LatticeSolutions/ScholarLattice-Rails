f03b68613248c9d327fd38f019c4bc2c69669626
Steven Clontz <steven.clontz@gmail.com>
2024-09-07 16:05:07 +0000

    tweak token generator

--- config/initializers/passwordless.rb
+++ config/initializers/passwordless.rb
@@ -3,4 +3,7 @@ Passwordless.configure do |config|
   config.success_redirect_path = "/collections" # After a user successfully signs in
   config.failure_redirect_path = "/users/sign_in" # After a sign in fails
   config.sign_out_redirect_path = "/" # After a user signs out
+  config.token_generator = lambda do |_|
+    [ *"A".."F", *"1".."9" ].sample(8).join
+  end
 end
