2ead61f352d683fdebcef239a283adb80fe38761
Steven Clontz <steven.clontz@gmail.com>
2024-09-15 22:09:30 +0000

    generate Profile scaffold

---
+++ app/models/profile.rb
@@ -0,0 +1,7 @@
+class Profile < ApplicationRecord
+  belongs_to :user
+  validates :email, presence: true, uniqueness: { scope: :user,
+    message: "A profile with this email address already exists for this user." }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_12_192444) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_15_213309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -71,6 +71,19 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_12_192444) do
     t.index ["identifier"], name: "index_passwordless_sessions_on_identifier", unique: true
   end
 
+  create_table "profiles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.string "affiliation"
+    t.string "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "email"], name: "index_profiles_on_user_id_and_email", unique: true
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email", null: false
     t.datetime "created_at", null: false
@@ -82,4 +95,5 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_12_192444) do
   add_foreign_key "likes", "collections"
   add_foreign_key "likes", "users"
   add_foreign_key "pages", "collections"
+  add_foreign_key "profiles", "users"
 end
