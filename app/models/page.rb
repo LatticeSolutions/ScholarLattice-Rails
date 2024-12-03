939c28a7918e4061774423fe34a6ed5a75a0523e
Steven Clontz <steven.clontz@gmail.com>
2024-12-03 03:22:16 +0000

    add page visibility migration and features

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,7 +2,8 @@ class Page < ApplicationRecord
   belongs_to :collection
   validates_uniqueness_of :is_home, if: :is_home, scope: :collection_id
   before_validation :enforce_single_homepage
-  enum :visibility, { private: 0, unlisted: 1, public: 2 }
+  enum :visibility, { private: 0, unlisted: 1, public: 2 }, suffix: true
+  validate :home_page_must_be_public
 
   def content_html
     Redcarpet::Markdown.new(Redcarpet::Render::HTML).render content
@@ -15,6 +16,12 @@ class Page < ApplicationRecord
         collection.pages.where.not(id: id).update(is_home: false)
         collection.pages.where(title: "Home Page").update(title: "Former Home Page")
         self.title = "Home Page"
+      end
+    end
+
+    def home_page_must_be_public
+      if is_home && !public_visibility?
+        errors.add(:visibility, "must be public for a home page")
       end
     end
 end
