bf387dbca7aec39af8d4d24a8b324ad16d48652b
Steven Clontz <steven.clontz@gmail.com>
2025-01-03 15:44:22 +0000

    refactor home page enforcement

--- app/models/page.rb
+++ app/models/page.rb
@@ -13,8 +13,10 @@ class Page < ApplicationRecord
 
     def enforce_single_homepage
       if is_home
-        collection.pages.where.not(id: id).update(is_home: false)
-        collection.pages.where(title: "Home Page").update(title: "Former Home Page")
+        collection.pages.where.not(id: id).where(is_home: true).update(
+          is_home: false,
+          title: "Former Home Page",
+        )
         self.title = "Home Page"
       end
     end
