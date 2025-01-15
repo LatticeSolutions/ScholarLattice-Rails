5a596bfb5c3bb8eefeacfa8a83f3641d56ad2537
Steven Clontz <steven.clontz@gmail.com>
2025-01-15 22:16:33 +0000

    add LaTeX support

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -3,6 +3,10 @@ class Submission < ApplicationRecord
   belongs_to :collection
   enum :status, { submitted: 0, accepted: 1, declined: 2, draft: 3 }
 
+  def abstract_html
+    Kramdown::Document.new(abstract).to_html
+  end
+
   def has_admin?(user)
     user.profiles.include? profile or
     collection.has_admin? user or
