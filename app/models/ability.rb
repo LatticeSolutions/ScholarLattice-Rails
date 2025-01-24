d1f28c6d23e0c1377944050cd15c69c1eac18573
Steven Clontz <steven.clontz@gmail.com>
2025-01-24 02:16:02 +0000

    add cancan abilities

---
+++ app/models/ability.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # default abilities
+    can :read, Collection
+    can :read, Page, visibility: [ :public, :unlisted ]
+    can :read, Profile
+    can :read, Submission, status: :accepted
+
+    return unless user.present?
+
+    can :manage, Collection do |c|
+      c.has_admin? user
+    end
+    cannot :destroy, Collection
+
+    can :manage, Page do |p|
+      p.collection.has_admin? user
+    end
+    cannot :destroy, Page
+
+    can :manage, Like, user: user
+    can :read, Like do |l|
+      l.collection.has_admin? user
+    end
+
+    can :manage, Submission do |s|
+      s.collection.has_admin?(user)
+    end
+    can :create, Submission do |s|
+      return false unless s.collection.submissions_open?
+      s.profile.in? user.profiles
+    end
+    can :update, Submission do |s|
+      s.profile.in? user.profiles
+    end
+    cannot :destroy, Submission
+
+    can :manage, Profile, user: user
+    cannot :destroy, Profile
+
+    return unless user.site_admin?
+    can :manage, :all
+  end
+end
