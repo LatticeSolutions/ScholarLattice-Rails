a933afad5f4706efde9bd8f165a1d34c881b65ba
Steven Clontz <steven.clontz@gmail.com>
2025-02-19 17:25:09 +0000

    help compare child/parent times

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,29 +3,27 @@ class Event < ApplicationRecord
   belongs_to :collection
   belongs_to :submission, optional: true
 
-  validate :starts_at_within_ancestor_range
-  validate :ends_at_within_ancestor_range
+  validate :starts_after_parent
+  validate :ends_before_parent
   validates :title, presence: true
+
+  def same_times_as_parent?
+    starts_at == parent&.starts_at && ends_at == parent&.ends_at
+  end
 
   private
 
-  def starts_at_within_ancestor_range
-    return unless starts_at.present?
-
-    ancestors.where.not(starts_at: nil).each do |ancestor|
-      unless starts_at >= ancestor.starts_at
-        errors.add(:starts_at, "must be within the time range of ancestor event #{ancestor.title}")
-      end
+  def starts_after_parent
+    return unless starts_at.present? && parent.present?
+    unless starts_at >= parent.starts_at
+      errors.add(:starts_at, "must start after parent #{parent.title} starts (#{parent.starts_at})")
     end
   end
 
-  def ends_at_within_ancestor_range
-    return unless ends_at.present?
-
-    ancestors.where.not(ends_at: nil).each do |ancestor|
-      unless ends_at <= ancestor.ends_at
-        errors.add(:ends_at, "must be within the time range of ancestor event #{ancestor.title}")
-      end
+  def ends_before_parent
+    return unless ends_at.present? && parent.present?
+    unless ends_at <= parent.ends_at
+      errors.add(:ends_at, "must end before parent #{parent.title} ends (#{parent.ends_at})")
     end
   end
 end
