bb9344513035d69d644d956c06c4db55e47b2b88
Steven Clontz <steven.clontz@gmail.com>
2025-02-22 00:01:31 +0000

    bugfix

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -93,20 +93,33 @@ class EventsController < ApplicationController
 
   # POST /events or /events.json
   def create_subevents
-    subevent_params = params.expect(:number_of_subevents, :length_of_each_subevent, :length_of_break, :collection_id)
-    subevent_params[0].to_i.times do |i|
+    number_of_subevents = params[:number_of_subevents]&.to_i || 0
+    length_of_each_subevent = params[:length_of_each_subevent]&.to_i || 0
+    length_of_break = params[:length_of_break]&.to_i || 0
+    collection = params[:collection_id].present? ? Collection.find(params[:collection_id]) : @event.collection
+    if number_of_subevents <= 0 || length_of_each_subevent <= 0 || length_of_break < 0
+      flash.now[:alert] = "Invalid input values. Please ensure all values are positive."
+      render :new_subevents, status: :unprocessable_entity
+      return
+    end
+    subevents = []
+    number_of_subevents.times do |i|
       subevent = @event.dup
+      @event.collection = collection
       subevent.title = "#{subevent.title} \##{i + 1}"
       subevent.parent = @event
       if @event.starts_at.present?
-        subevent.starts_at = @event.starts_at + i * subevent_params[1].to_i.minutes + i * subevent_params[2].to_i.minutes
-        subevent.ends_at = subevent.starts_at + subevent_params[1].to_i.minutes
+        subevent.starts_at = @event.starts_at + i * length_of_each_subevent.minutes + i * length_of_break.minutes
+        subevent.ends_at = subevent.starts_at + length_of_each_subevent.minutes
       end
-      unless subevent.save
-        redirect_to @event, notice: "Warning: some subevents could not be created."
-        return
-      end
+      subevents << subevent
     end
+    if subevents.any?(&:invalid?)
+      flash.now[:alert] = "Some subevents could not be created."
+      render :new_subevents, status: :unprocessable_entity
+      return
+    end
+    subevents.each(&:save!)
     redirect_to @event, notice: "All subevents were successfully created."
   end
 
