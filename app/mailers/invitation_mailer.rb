08a2695028c519da2e407a7ec66dd97844a4cf52
Steven Clontz <steven.clontz@gmail.com>
2025-03-25 02:04:41 +0000

    fix invitation email subject

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,6 +1,6 @@
 class InvitationMailer < ApplicationMailer
   def invitation_created(invitation)
     @invitation = invitation
-    mail(to: invitation.notification_emails, subject: "Submission received by ScholarLattice")
+    mail(to: invitation.notification_emails, subject: "Invitation to submit to #{invitation.collection.name} on ScholarLattice")
   end
 end
