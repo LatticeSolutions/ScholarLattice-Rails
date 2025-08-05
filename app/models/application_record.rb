dec96a422d27510b0cd790c9523d202a30b48abc
Steven Clontz <steven.clontz@gmail.com>
2025-08-05 23:33:41 +0000

    add inherited method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
+  def inherited(attribute)
+    public_send(attribute).present? ? public_send(attribute) : parent&.inherited(attribute)
+  end
+
   private
 
   def downcase_email
