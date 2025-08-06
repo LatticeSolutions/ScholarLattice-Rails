8b3081a50801d1a4a2d57abec248239457e294e4
Steven Clontz <steven.clontz@gmail.com>
2025-08-06 17:14:15 +0000

    using brakeman ignore

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -62,7 +62,6 @@ class EventsController < ApplicationController
       return
     end
     authorize! :access_webinar, @event, message: "Must have an accepted registration to #{@event.collection.title} to access this webinar."
-    # brakeman:ignore Redirect
     redirect_to @event.inherited(:webinar_link), allow_other_host: true
   end
 
