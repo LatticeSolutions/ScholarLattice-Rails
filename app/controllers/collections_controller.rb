f8f4f568cac40c7a9bbaea0cc75dea08e9ed5912
Steven Clontz <steven.clontz@gmail.com>
2025-09-08 04:27:47 +0000

    implement alias

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -82,7 +82,7 @@ class CollectionsController < ApplicationController
       params.require(:collection).permit(
         :title, :short_title, :description, :parent_id, :subcollection_name,
         :submittable, :admin_emails, :time_zone, :submissions_open_on, :submissions_close_on,
-        :order, :show_events, :registerable, :public_webinars,
+        :order, :show_events, :registerable, :public_webinars, :affiliation_identifier_alias
       )
     end
 
