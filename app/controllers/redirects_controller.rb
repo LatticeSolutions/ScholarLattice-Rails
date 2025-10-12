f471d1b81d7934afd3041ef10268c0fa2fd85b69
Steven Clontz <steven.clontz@gmail.com>
2025-10-12 02:11:57 +0000

    fix

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -1,5 +1,5 @@
 class RedirectsController < ApplicationController
   def index
-    redirect_to "/", status: :moved_permanently, allow_other_host: true
+    redirect_to "/", subdomain: nil, status: :moved_permanently, allow_other_host: true
   end
 end
