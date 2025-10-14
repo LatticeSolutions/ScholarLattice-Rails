77c7120437a8fa11e858ca7acc2a87c363887206
Steven Clontz <steven.clontz@gmail.com>
2025-10-14 02:21:23 +0000

    factor out redirect checker

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -17,16 +17,4 @@ class RedirectsController < ApplicationController
       render file: Rails.root.join("public/404.html"), status: :not_found, layout: false
     end
   end
-
-  private
-
-  def redirect_to_if_allowed(url)
-    allowed_domains = [ "scholarlattice.org" ]
-    uri = URI.parse(url) rescue nil
-    if uri.present? && allowed_domains.include?(uri.host)
-      redirect_to url, status: :moved_permanently, allow_other_host: true
-    else
-      redirect_to(root_url, alert: "Redirects to host `#{uri.host}` are not supported.")
-    end
-  end
 end
