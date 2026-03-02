21d0361c1b366763e95fdb6256d805a09fa37ebc
Steven Clontz <steven.clontz@gmail.com>
2026-03-02 20:59:31 +0000

    order registration option choices by option

--- app/models/registration_option_choice.rb
+++ app/models/registration_option_choice.rb
@@ -4,6 +4,8 @@ class RegistrationOptionChoice < ApplicationRecord
   validates :registration_id, uniqueness: { scope: :registration_option_id }
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  default_scope { joins(:registration_option).order("registration_options.name ASC") }
+
   def has_cost?
     registration_option.cost.present?
   end
