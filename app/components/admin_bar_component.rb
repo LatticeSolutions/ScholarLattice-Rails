4fc588ef7398b027059b4e447f0503fd16e403f0
Steven Clontz <steven.clontz@gmail.com>
2026-04-13 19:33:24 +0000

    lots of work...

--- app/components/admin_bar_component.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AdminBarComponent < ViewComponent::Base
-  def initialize(*links)
-    @links = links
-    @links.map! { |l| l[:color] = "purple-800" unless l[:color]; l }
-  end
-end
---
+++ app/components/menu_toggle_component.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class MenuToggleComponent < ViewComponent::Base
+  def initialize(menu_text, menu_icon, button, *links)
+    @menu_id = "toggle-menu-#{SecureRandom.hex(8)}"
+    @menu_text = menu_text
+    @menu_icon = menu_icon
+    @button = button
+    @links = links
+  end
+end
