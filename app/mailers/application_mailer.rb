a49ef09dfb5dbbd57896b89390adfa0e0b235bc6
Steven Clontz <steven.clontz@gmail.com>
2026-05-06 21:35:42 +0000

    again stub out images since they aren't working yet

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,12 +2,12 @@ class ApplicationMailer < ActionMailer::Base
   default from: email_address_with_name("updates@mailer.scholarlattice.org", "ScholarLattice Updates")
   default reply_to: "support@scholarlattice.org"
   layout "mailer"
-  before_action :attach_logo
+  # before_action :attach_logo
 
   private
 
-  def attach_logo
-    attachments.inline["logo.png"] = File.read(Rails.root.join("app/assets/images/logo.png"))
-    attachments["logo.png"].header["content-id"] = "site.logo"
-  end
+  # def attach_logo
+  #   attachments.inline["logo.png"] = File.read(Rails.root.join("app/assets/images/logo.png"))
+  #   attachments["logo.png"].header["content-id"] = "site.logo"
+  # end
 end
